dd5afb071befb9f5df6ff595accdccccc7a84bf3
lucioceaalvaro <91050860+lucioceaalvaro@users.noreply.github.com>
2021-12-28 13:56:20 +0100

    V.2.2

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,8 +3,9 @@ import {Request, Response, Router } from 'express'
 import { Habitaciones,tHabitaciones } from '../Schema/HabiSchema'
 import {Empleado, tEmpleado} from '../Schema/EmpSchema'
 import { Reservas, tReservas } from '../Schema/ResSchema';
-import { Iclientes } from '../Schema/CliSchema';
+import { Clientes, Iclientes } from '../Schema/CliSchema';
 import { db } from '../database/database'
+import { Cliente } from '../clases/Cliente';
 
 class DatoRoutes {
     private _router: Router
@@ -28,6 +29,112 @@ class DatoRoutes {
         })
 
         db.desconectarBD()
+    }
+    private getClientes = async (req: Request, res: Response) => {
+        await db.conectarBD()
+        .then( async (mensaje) => {
+            console.log(mensaje)
+            const query  = await Clientes.find({})
+            res.json(query)
+        })
+        .catch((mensaje) => {
+            res.send(mensaje)
+        })
+
+        db.desconectarBD()
+    }
+    private getEmpleados = async (req: Request, res: Response) => {
+        await db.conectarBD()
+        .then( async (mensaje) => {
+            console.log(mensaje)
+            const query  = await Empleado.find({})
+            res.json(query)
+        })
+        .catch((mensaje) => {
+            res.send(mensaje)
+        })
+
+        db.desconectarBD()
+    }
+    private getReservas = async (req: Request, res: Response) => {
+        await db.conectarBD()
+        .then( async (mensaje) => {
+            console.log(mensaje)
+            const query  = await Reservas.find({})
+            res.json(query)
+        })
+        .catch((mensaje) => {
+            res.send(mensaje)
+        })
+
+        db.desconectarBD()
+    }
+    private postHabitacion = async (req: Request, res: Response) => {
+        const { tipoObjeto,idHab,camas,pnoche,estado,desayuno,supletonia,spa } = req.body
+        await db.conectarBD()
+        const dSchema={
+            _tipoObjeto:tipoObjeto,
+            _IdHab:idHab,
+            _Camas:camas,
+            _PNoche:pnoche,
+            _estado:estado,
+            _desayuno:desayuno,
+            _supletonia:supletonia,
+            _spa:spa
+        }
+        const oSchema = new Habitaciones(dSchema)
+        await oSchema.save()
+            .then( (doc: any) => res.send(doc))
+            .catch( (err: any) => res.send('Error: '+ err)) 
+        await db.desconectarBD()
+    }
+    private postEmpleado = async (req: Request, res: Response) => {
+        const {tipo,dni,salarioBase,titulacion,Nestrellas,habitaciones,nocturnidad} = req.body
+        await db.conectarBD()
+        const dSchema={
+           _tipoObjeto:tipo,
+           _dni:dni,
+           _salarioBase:salarioBase,
+           _Titulacion:titulacion,
+           _NEstrella:Nestrellas,
+           _habitationes:habitaciones,
+           _Nocturnidad:nocturnidad,
+        }
+        const oSchema = new Empleado(dSchema)
+        await oSchema.save()
+            .then( (doc: any) => res.send(doc))
+            .catch( (err: any) => res.send('Error: '+ err)) 
+        await db.desconectarBD()
+    }
+    private postCliente = async (req: Request, res: Response) => {
+        const {dni,nombre,nTarjeta} = req.body
+        await db.conectarBD()
+        const dSchema={
+            _dni: dni,
+            _nombre: nombre,
+            nTarjeta: nTarjeta
+        }
+        const oSchema = new Clientes(dSchema)
+        await oSchema.save()
+            .then( (doc: any) => res.send(doc))
+            .catch( (err: any) => res.send('Error: '+ err)) 
+        await db.desconectarBD()
+    }
+    private postReservas = async (req: Request, res: Response) => {
+        const {clientes,nDias,habitacion,nPersonas,precio} = req.body
+        await db.conectarBD()
+        const dSchema={
+            _clientes: clientes,
+            _nDias: nDias,
+            _habitacion:habitacion,
+            _nPersonas: nPersonas,
+            _precio: precio   
+        }
+        const oSchema = new Reservas(dSchema)
+        await oSchema.save()
+            .then( (doc: any) => res.send(doc))
+            .catch( (err: any) => res.send('Error: '+ err)) 
+        await db.desconectarBD()
     }
 
     // private getAuto = async (req: Request, res: Response) => {
@@ -164,13 +271,25 @@ class DatoRoutes {
 
 
     misRutas(){
+        //GET
         this._router.get('/habitaciones', this.getHabitaciones)
+        this._router.get('/clientes', this.getClientes)
+        this._router.get('/empleados', this.getEmpleados)
+        this._router.get('/reservas', this.getReservas)
+        //POST
+        this._router.post('/habitaciones', this.postHabitacion)
+        this._router.post('/empleados', this.postEmpleado)
+        this._router.post('/clientes', this.postCliente)
+        this._router.post('/reservas', this.postReservas)
         // this._router.get('/autos/:valor', this.getAuto)
         // this._router.post('/auto', this.crearAuto)
         // this._router.put('/modificar', this.modificarAuto)
         // this._router.put('/mod/:matriculaP/:cambioP', this.modificarAuto2)
         // this._router.delete('/auto/:matricula', this.deleteAutos)
         //this._router.put('/autos/:matriculax/:cambio', this.updatePm)
+        //UPDATE
+        
+        //DELETE
     }
 
 }
